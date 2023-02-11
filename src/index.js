dc209a8d63f6c4eee87e2ea74ad83697e1d5bf90
Anna Sova <sova.ania@gmail.com>
2023-02-11 16:37:17 +0100

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,76 @@
+import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.getElementById('search-box');
+const countriesList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+searchBox.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry() {
+  const value = searchBox.value.trim();
+  fetchCountries(value)
+    .then(country => {
+      if (country.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        resetListAndInfo();
+        // return (markup = '');
+      }
+      if (country.length > 1 && country.length < 11) {
+        markup = country.map(createCountriesList).join('');
+        return updateCountriesList(markup);
+      }
+      if (country.length === 1) {
+        markup = country.map(createCountryInfo).join('');
+        return updateCountryInfo(markup);
+      }
+    })
+    .catch(onError);
+}
+
+function resetListAndInfo() {
+  countriesList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
+
+function updateCountriesList(markup) {
+  countriesList.innerHTML = markup;
+  countryInfo.innerHTML = '';
+}
+
+function updateCountryInfo(markup) {
+  countryInfo.innerHTML = markup;
+  countriesList.innerHTML = '';
+}
+
+function createCountriesList({ name: { official }, flags: { svg } }) {
+  return `<li class="country-item"> <img width="30" height="20" src=${svg} />
+     <span> ${official}</span></li>`;
+}
+
+function createCountryInfo({
+  name: { official },
+  capital,
+  population,
+  flags: { svg },
+  languages,
+}) {
+  return `
+      <p> <img width="30" height="20" src=${svg} />
+      <span class="country-name">${official}</span></p>
+      <p class="country-field"> <span>Capital:</span> ${capital}</p>
+      <p class="country-field"> <span>Population:</span> ${population}</p>
+      <p class="country-field"> <span>Languages:</span> ${Object.values(languages)}</p>
+    `;
+}
+
+function onError(err) {
+  console.error(err);
+  Notify.failure('Oops, there is no country with that name');
+}
